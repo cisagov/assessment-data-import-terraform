e86586912708fdec30096c7c904b16b61fcc43e7
Dave Redmin <david.redmin@beta.dhs.gov>
2019-04-26 13:35:51 -0400

    Make bucket and user names include terraform workspace name

--- exported_data_bucket.tf
+++ exported_data_bucket.tf
@@ -1,7 +1,10 @@
 resource "aws_s3_bucket" "exported_data" {
   # This bucket is used to store a JSON file containing all exported
-  # assessment data to be imported into our AWS database
-  bucket = "${format("assessment-export-data-%s", terraform.workspace)}"
+  # assessment data to be imported into our AWS database.
+  # Note that in production terraform workspaces, the string '-production' is
+  # appended to the bucket name.  In non-production workspaces,
+  # '-<workspace_name>' is appended to the bucket name.
+  bucket = "${local.production_workspace ? format("%s-production", var.assessment_data_s3_bucket) : format("%s-%s", var.assessment_data_s3_bucket, terraform.workspace)}"
 
   server_side_encryption_configuration {
     rule {
--- exported_data_user_delete.tf
+++ exported_data_user_delete.tf
@@ -1,7 +1,8 @@
 resource "aws_iam_user" "exported_data_delete" {
-  # We name the user assessment-export-data-delete-<workspace_name> to avoid
-  # name conflicts when deploying to different workspaces
-  name = "${format("assessment-export-data-delete-%s", terraform.workspace)}"
+  # We name the user based on the bucket name (which is based on the
+  # terraform workspace name) to avoid name conflicts when deploying to
+  # different workspaces
+  name = "${format("%s-delete-%s", aws_s3_bucket.exported_data.id, terraform.workspace)}"
 
   lifecycle {
     prevent_destroy = true
