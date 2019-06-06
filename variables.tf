8638b8f58d5fe70cc3a0d14e932ee1dd3f4c3b51
Dave Redmin <david.redmin@beta.dhs.gov>
2019-06-06 17:20:16 -0400

    Run terraform 0.12upgrade
    These changes are all from running terraform 0.12upgrade script.

--- variables.tf
+++ variables.tf
@@ -9,19 +9,19 @@ variable "aws_availability_zone" {
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   description = "Tags to apply to all AWS resources created"
   default     = {}
 }
 
 variable "assessment_data_s3_bucket" {
-  type        = "string"
+  type        = string
   description = "The name of the bucket where the assessment data JSON file will be stored.  Note that in production terraform workspaces, the string '-production' will be appended to the bucket name.  In non-production workspaces, '-<workspace_name>' will be appended to the bucket name."
   default     = ""
 }
 
 variable "assessment_data_import_lambda_s3_bucket" {
-  type        = "string"
+  type        = string
   description = "The name of the bucket where the assessment data import lambda function will be stored.  Note that in production terraform workspaces, the string '-production' will be appended to the bucket name.  In non-production workspaces, '-<workspace_name>' will be appended to the bucket name."
   default     = ""
 }
